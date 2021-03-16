1c22e73d7b55c5e0cdbe9876e14af6c8a9ca4ded
Sean Hubbard <2468513H@student.gla.ac.uk>
2021-03-16 20:22:03 +0000

    register and login added

--- team9a/evaluArt/urls.py
+++ team9a/evaluArt/urls.py
@@ -5,4 +5,6 @@ app_name = 'evaluArt'
 
 urlpatterns = [
     path('', views.base, name='base'),
+    path('register/', views.register, name='register'), # New mapping
+    path('login/', views.user_login, name='login'),
 ]
