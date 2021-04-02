879f4960a4eac68b30f50f79984d7251dd20c6c5
Sean Hubbard <2468513H@student.gla.ac.uk>
2021-04-02 21:58:41 +0100

    category slug test

--- team9a/evaluArt/tests.py
+++ team9a/evaluArt/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from evaluArt.models import Category
 
 # Create your tests here.
+class CategoryMethodTests(TestCase):
+    def test_slug_line_creation(self):
+        """
+        Checks to make sure that when a category is created, an
+        appropriate slug is created.
+        Example: "Random Category String" should be "random-category-string".
+        """
+        category = Category(name='Random Category String')
+        category.save()
+        self.assertEqual(category.slug, 'random-category-string')
